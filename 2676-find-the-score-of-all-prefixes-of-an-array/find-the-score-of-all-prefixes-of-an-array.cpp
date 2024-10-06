02c9211455ddbd60c1ff592a3512025d4a49fbf0
neil0604 <snehildey13@gmail.com>
2024-10-06 13:06:02 +0530

    Time: 107 ms (11.94%) | Memory: 61.2 MB (47.21%) - LeetSync

---
+++ 2676-find-the-score-of-all-prefixes-of-an-array/find-the-score-of-all-prefixes-of-an-array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<long long> findPrefixScore(vector<int>& nums) {
+        int n=nums.size();
+        vector<long long> cover(n);
+        int max1=-10000;
+        vector<long long> pre(n);
+        for(int i=0;i<n;i++){
+            
+
+            max1=max(max1,nums[i]);
+            cover[i]=nums[i]+max1;
+
+            }
+            pre[0]=cover[0];
+        for(int i=1;i<cover.size();i++){
+            pre[i]=pre[i-1]+cover[i];
+        }  
+
+        return pre;  
+        }
+    
+};
