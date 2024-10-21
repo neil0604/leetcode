b123f3c07a83aceb684d9414e4fb25a16bdd3dec
neil0604 <snehildey13@gmail.com>
2024-10-21 19:47:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (70.63%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        string ans;
+        int a=num;
+        int arr[]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        string ch[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+           
+            for(int i=0;i<13;i++){
+                while(a >= arr[i]){
+                 a=a-arr[i];
+                ans=ans+ch[i];
+               
+            }
+            
+            }
+        
+        return ans;
+    }
+};
