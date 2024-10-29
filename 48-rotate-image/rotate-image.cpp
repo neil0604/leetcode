9f648473a2783820933c8f937305b1d1b4796aa0
neil0604 <snehildey13@gmail.com>
2024-10-29 14:49:30 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (10.96%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& a) {
+        for(int i=0;i<a.size();i++){
+            for(int j=i+1;j<a[0].size();j++){
+                
+               // if(i!=j) 
+                swap(a[i][j],a[j][i]);
+            }
+        }
+        for(int k=0;k<a.size();k++){
+            int i1=0;
+            int j1=a.size()-1;
+            while(i1<j1){
+                swap(a[k][i1],a[k][j1]);
+                i1++;
+                j1--;
+            }
+        }
+      //  return a;
+    }
+};
