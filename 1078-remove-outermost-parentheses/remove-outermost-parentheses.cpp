e01e7db52188be98a4b5511486b82ac06987839c
neil0604 <snehildey13@gmail.com>
2024-11-02 09:52:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (10.78%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,31 +1,25 @@
+
 class Solution {
 public:
-    // Function to remove outermost parentheses of every primitive string in the
-    // decomposition of s
     string removeOuterParentheses(string s) {
-        string result;   // To store the final result
-        int balance = 0; // To keep track of the balance of parentheses
-
-        // Iterate through each character in the string
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                // If balance is greater than 0, it means this '(' is not an
-                // outermost parenthesis
-                if (balance > 0) {
-                    result += s[i]; // Add the character to the result
-                }
-                balance++; // Increase the balance for '('
-            } else {
-                balance--; // Decrease the balance for ')'
-                // If balance is greater than 0, it means this ')' is not an
-                // outermost parenthesis
-                if (balance > 0) {
-                    result += s[i]; // Add the character to the result
-                }
+        string result;
+        stack<char> stack;
+        int start = 0; 
+        int n = s.size();
+        for(int i = 0; i <n; i++){
+            if(s[i] == '(') {
+                stack.push('(');
+            }else{
+                stack.pop();
+            }
+           
+            if(stack.empty()){
+              
+                result += s.substr(start + 1, i - start - 1);
+                start = i + 1;  
             }
         }
 
-        return result; // Return the final result after removing outermost
-                       // parentheses
+        return result;
     }
 };
