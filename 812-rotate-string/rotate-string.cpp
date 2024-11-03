eba2b18f453a8933fbc2a73445cf4b3c72ad0b2f
neil0604 <snehildey13@gmail.com>
2024-11-03 17:59:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (10.19%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        int n=s.size();
+        int i=0;
+       string str=s;
+        if(s==goal) return true;
+        while(n>0){
+            
+            str=str.substr(1);
+            
+            char ch=s[i];
+            str+=ch;
+           // cout<<str<<endl;
+            if(str==goal) return true;
+            n--;
+            i++;
+
+
+        }
+        return false;
+    }
+};
