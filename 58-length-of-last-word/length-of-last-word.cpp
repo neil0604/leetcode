921bc23c7cbfe11014840da4e6ebac9f0628c7a6
neil0604 <snehildey13@gmail.com>
2024-12-09 18:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (6.51%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string temp="";
+        while(ss >> temp){
+
+        }
+        int n=temp.size();
+        return n;
+        
+    }
+};
