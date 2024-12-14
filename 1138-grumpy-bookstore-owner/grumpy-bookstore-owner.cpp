eda270a96153e7bb01678bb6dea945880dd85c13
neil0604 <snehildey13@gmail.com>
2024-12-15 00:08:23 +0530

    Time: 3 ms (30.87%) | Memory: 35.7 MB (23.61%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int n = customers.size();
-        int sum = 0;
-        int maxSum = 0;
-        int windowSum = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (grumpy[i] == 0) {
-                sum += customers[i];
-            }
-            windowSum += customers[i] * grumpy[i];
-
-            if (i >= minutes) {
-                windowSum -= customers[i - minutes] * grumpy[i - minutes];
-            }
-
-            maxSum = max(maxSum, windowSum);
+        
+        int k=minutes;
+        int previousloss=0;
+        //finding loss of satisfaction
+        for(int i=0;i<k;i++){
+            if(grumpy[i]==1)
+            previousloss+=customers[i];
         }
-
-        return sum + maxSum;
+        int i=1;
+        int j=k;
+       int maxloss=previousloss;
+       int currentloss=0;
+       int n=grumpy.size();
+      int start=0;
+       int last=k;
+       while(j<n){
+            currentloss=previousloss;
+            if(grumpy[i-1]==1) currentloss-=customers[i-1];
+            if(grumpy[j]==1) currentloss+=customers[j];
+            if(currentloss>maxloss){
+                maxloss=currentloss;
+                start=i;
+                last=j;
+                cout<<start<<" "<<last<<endl;
+                            }
+            previousloss=currentloss;
+            i++;
+            j++;
+       }
+       //filling zeros in grumpy;
+       for(int i=start;i<k+start;i++){
+            grumpy[i]=0;
+       }
+       int sum=0;
+       for(int i=0;i<n;i++){
+           // grumpy[i]=0;
+           if(grumpy[i]==0)
+           sum+=customers[i];
+       }
+       return sum;
     }
 };
