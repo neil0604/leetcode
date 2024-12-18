15f3a915a1bd0e30c78249a6d22ddb025ca86df7
neil0604 <snehildey13@gmail.com>
2024-12-18 20:14:30 +0530

    Time: 1 ms (23.39%) | Memory: 14.1 MB (24.39%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> finalPrices(vector<int>& p) {
         int n=p.size();
-        vector<int> v(n);
+       // vector<int> v(n);
         for(int i=0;i<n-1;i++){
             int j=i+1;
             while(j < n && p[i] < p[j]){
