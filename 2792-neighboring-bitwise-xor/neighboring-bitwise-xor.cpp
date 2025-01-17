eac1ecb9f925b6b20072249ddd97247c431911f0
neil0604 <snehildey13@gmail.com>
2025-01-18 01:25:18 +0530

    Time: 0 ms (100.00%) | Memory: 264 MB (50.63%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool doesValidArrayExist(vector<int>& derived) {
+        int n=derived.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum^derived[i];
+        }
+        if(sum==0) return true;
+        else return false;
+    }
+};
