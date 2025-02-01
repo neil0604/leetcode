78c6ba53fe4538a4348769b12fb665fd93160d86
neil0604 <snehildey13@gmail.com>
2025-02-01 21:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (25.00%) - LeetSync

---
+++ 3429-special-array-i/special-array-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int i=0;
+        int j=1;
+        int n=nums.size();
+        while(j<n){
+            if(nums[j]%2==0 && nums[i]%2==1){
+                j++;
+            i++;
+                continue;
+            }else if(nums[j]%2==1 && nums[i]%2==0){
+                j++;
+            i++;
+                continue;
+            }else return false;
+            
+        }
+        return true;
+    }
+};
