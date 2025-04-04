f24975d377ddcde969c629d2459290865afc2f95
neil0604 <snehildey13@gmail.com>
2025-04-04 12:21:33 +0530

    Time: 3 ms (14.06%) | Memory: 18.9 MB (5.96%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,39 +1,31 @@
 class Solution {
-public:
-    vector<vector<int>> floodFill(vector<vector<int>>& im, int sr, int sc, int c) {
+private:
+    void dfs(vector<vector<int>> visited,vector<vector<int>>& im,int sr,int sc,int color){
          int n=im.size();
         int m=im[0].size();
-queue<pair<int,int>> q;
-        int ini = im[sr][sc];
-    
-        im[sr][sc]=c;
-        q.push({sr,sc});
-        vector<vector<int>> vis(n,vector<int> (m,0));
-        vis[sr][sc]=1;
-       
-
-        while(!q.empty()){
-
-            int delrow[]={-1,0,1,0};
+        visited[sr][sc]=1;
+        int ini=im[sr][sc];
+        im[sr][sc]=color;
+           int delrow[]={-1,0,1,0};
             int delcol[]={0,1,0,-1};
-
-          sr=  q.front().first;
-            sc=q.front().second;
-            q.pop();
-
-            for(int i =0;i<4;i++){
-                int nrow=sr+delrow[i];
+        for(int i=0;i<4;i++){
+             int nrow=sr+delrow[i];
                 int ncol=sc+delcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && im[nrow][ncol]==ini && vis[nrow][ncol]==0){
-                    im[nrow][ncol]=c;
-                    vis[nrow][ncol]=1;
-                    q.push({nrow,ncol});
-
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && im[nrow][ncol]==ini && visited[nrow][ncol]==0){
+                
+                dfs(visited,im,nrow,ncol,color);
+                
                 }
-            }
-
         }
-
-        return im;
     }
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& im, int sr, int sc, int c) {
+          int n=im.size();
+        int m=im[0].size();
+vector<vector<int>> vis(n,vector<int> (m,0));
+        dfs(vis,im,sr,sc,c);
+        return im;
+
+    }
+       
 };
