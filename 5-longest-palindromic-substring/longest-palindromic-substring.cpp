b68a650e25547a440b0e4c87667609fbaa7d80e5
neil0604 <snehildey13@gmail.com>
2025-07-04 13:11:54 +0530

    Time: 318 ms (18.22%) | Memory: 18.8 MB (46.00%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -15,42 +15,68 @@ public:
         }
 
         for(int i=1;i<n;i++){
+           bool flag=false;
             cout<<endl<<"i "<<i<<endl;
             int low=i;
             int high = i;
             while(s[low]==s[high]){
+                flag=true;
                 low--;
                 high++;
+                
 
-                if(low==-1 || high==n) break;
+                if(low==-1 || high==n) {
+                    low++;
+                    high--;
+                    break;}
+
+                    if(s[low]!=s[high]){
+                    low++;
+                    high--;
+                    break;
+                }
 
             }
-            int no = (high-1)-(low+1)+1;
-            string palindrome = str.substr(low+1,no);
+            if(flag)
+            {int no = (high)-(low)+1;
+            string palindrome = str.substr(low,no);
             int size = palindrome.size();
-            if(size >= result.size()){
+            if(size > result.size()){
                 result=palindrome;
                 cout<<result<<" "<<low<<" " <<high;
             }
-
+}
 
             //for cases like dd(even)
             low=i-1;
             high=i;
+            flag=false;
             while(s[low]==s[high]){
                 low--;
                 high++;
+                flag=true;
+               
+                 if(low==-1 || high==n) {
+                    low++;
+                    high--;
+                    break;}
 
-                 if(low==-1 || high==n) break;
+                 if(s[low]!=s[high]){
+                    low++;
+                    high--;
+                    break;
+                }
             }
-            no = (high-1)-(low+1)+1;
-            palindrome = str.substr(low+1,no);
-             size = palindrome.size();
+            if(flag){
+           int no = (high)-(low)+1;
+          string  palindrome = str.substr(low,no);
+             int size = palindrome.size();
 
-              if(size >= result.size()){
+              if(size > result.size()){
                 result=palindrome;
                 cout<<endl<<"2)"<<result<<" "<<low<<" "<<high<<endl;
             }
+        }
 
         }
         return result;
