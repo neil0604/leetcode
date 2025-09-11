8c084988a96c078ce12b5d80677aece7f4c47df7
neil0604 <snehildey13@gmail.com>
2025-09-11 17:48:49 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (58.56%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -9,13 +9,14 @@ public:
         for(int i=0;i<m;i++){
 
             for(int j=0;j<n;j++){
-                int left=0,up=0;
+                int right=0,down=0;
                 if(i==0 && j==0)  dp[0][0]=1;
+                if(i==0 || j==0)  dp[i][j]=1;
+                
               else{
-              if(j>0)  left = dp[i][j-1];
-              if(i>0)  up = dp[i-1][j];
-
-              dp[i][j]=up+left;
+                right = dp[i][j-1];
+                down = dp[i-1][j];
+                dp[i][j]=down+right;
               }
               
             }
