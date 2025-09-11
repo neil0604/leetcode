129407f6d963b9b7361d0bdf2c25879753eeee65
neil0604 <snehildey13@gmail.com>
2025-09-12 03:59:55 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (5.08%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -5,22 +5,25 @@ public:
         //tabulation
         int n=grids.size();
         int m=grids[0].size();
+        vector<int> prev(m,0);
+       
         vector<vector<int>> dp(n,vector<int> (m,0));
         for(int i=0;i<n;i++){
-
+            vector<int> curr(m,0);
             for(int j=0;j<m;j++){
-                if(i==0 && j==0) dp[0][0]=grids[0][0];
+                if(i==0 && j==0) curr[0]=grids[0][0];
                else{
                  int Up=1e9,left=1e9;
-              if(i>0)  Up=grids[i][j]+dp[i-1][j];
-              if(j>0)  left=grids[i][j] + dp[i][j-1];
+              if(i>0)  Up=grids[i][j]+prev[j];
+              if(j>0)  left=grids[i][j] + curr[j-1];
 
-              dp[i][j]=min(Up,left);
+              curr[j]=min(Up,left);
                }
             }
+            prev=curr;
         }
 
-        return dp[n-1][m-1];
+        return prev[m-1];
         
     }
 };
