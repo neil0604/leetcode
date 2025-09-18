665f7975f13477280345aeba8be600af91bd14ae
neil0604 <snehildey13@gmail.com>
2025-09-18 13:10:09 +0530

    Time: 2 ms (57.25%) | Memory: 20.1 MB (8.28%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<int> v(k);
-        unordered_map<int,int> mp;
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        int v1=mp.size();
-        for(int i=0;i<v1;i++){
-            int fre=0;
-            int key;
-            int mx=INT_MIN;
-            cout<<"i  "<<i<<endl;
-            for(auto ele1 : mp){
-             fre=ele1.second;
-            if(mx<fre){
-                 key=ele1.first;
-                 cout<<key<<endl;
-                 mx=fre;
-                
-            }
-            }
-             mp[key]=0;
-            if(k>0) v[--k]=key;
-
-
-            }
-            return v;
-
-        }
-
         
-    
+
+        int n=nums.size();
+        unordered_map<int,int> mp;
+        for(auto ele : nums){
+            mp[ele]++;
+        }
+        // store {count,list of all elements with same count}
+        vector<vector<int>> ans(n+1,vector<int> ());
+        for(auto ele : mp){
+            ans[ele.second].push_back(ele.first);
+        }
+        vector<int> result;
+        for(int i=n;i>=0;i--){
+
+            for(auto child : ans[i]){
+                if(k==0) {
+                    return result;
+                }
+                result.push_back(child);
+                k--;
+            }
+            
+        }
+        return result;
+    }
 };
