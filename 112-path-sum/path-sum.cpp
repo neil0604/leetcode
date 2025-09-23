5a5bdbcb5f13d656680a2a410bf455c8fc089b41
neil0604 <snehildey13@gmail.com>
2025-09-24 03:11:42 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (35.13%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -10,20 +10,23 @@
  * };
  */
 class Solution {
-public:
-    bool helper(TreeNode* root,int target){
-        if(root==NULL) return false;
-         if(target==root->val && root->left==NULL && root->right==NULL) return true;
-       // if(target-root->val<0) return false;
-      bool a =  helper(root->left,target-root->val);
-       
-     bool  b =   helper(root->right,target-root->val);
-     
+private:
+    bool f(TreeNode* root,int sum,int target){
+        if(root->left==NULL && root->right==NULL){
+          if(  sum+root->val==target) return true;
+          return false;
+        }
+        bool left=false,right=false;
+      if(root->left!=NULL) left= f(root->left,sum+root->val,target);
+      if(root->right!=NULL) right= f(root->right,sum+root->val,target);
 
-     return a || b;
-
+      if(left==true || right==true) return true;
+      return false;
     }
+public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-       return helper(root,targetSum);
+        if(root==NULL) return false;
+        int sum=0;
+       return f(root,sum,targetSum);
     }
 };
