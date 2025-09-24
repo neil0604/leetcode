db23d63fba396b6b4f5cc1464e3e1095fde1a2a0
neil0604 <snehildey13@gmail.com>
2025-09-25 02:58:44 +0530

    Time: 8 ms (5.55%) | Memory: 11.9 MB (89.08%) - LeetSync

--- 133-clone-graph/clone-graph.cpp
+++ 133-clone-graph/clone-graph.cpp
@@ -1,32 +1,60 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
 
 class Solution {
-    public:
-    Node* dfs(Node* cur,unordered_map<Node*,Node*>& mp)
-    {
-        vector<Node*> neighbour;
-        Node* clone=new Node(cur->val);
-        mp[cur]=clone;
-            for(auto it:cur->neighbors)
-            {
-                if(mp.find(it)!=mp.end())   //already clone and stored in map
-                {
-                    neighbour.push_back(mp[it]);    //directly push back the clone node from map to neigh
-                }
-                else
-                    neighbour.push_back(dfs(it,mp));
-            }
-            clone->neighbors=neighbour;
-            return clone;
-    }
+public:
     Node* cloneGraph(Node* node) {
+        // bfs using
+        if(node==NULL) return node;
+        Node* copy=new Node(node->val);
         unordered_map<Node*,Node*> mp;
-        if(node==NULL)
-            return NULL;
-        if(node->neighbors.size()==0)   //if only one node present no neighbors
-        {
-            Node* clone= new Node(node->val);
-            return clone; 
+
+        //applying bfs
+
+        queue<Node*> q;
+        q.push(node);
+        mp[node] = copy;
+
+
+        while(!q.empty()){
+            Node* ori=q.front();
+            q.pop();
+
+            Node* tempCopy=mp[ori];
+
+            for(Node* n : ori->neighbors){
+                if(mp.find(n)==mp.end()){
+                    Node* clone=new Node(n->val);
+                    mp[n]=clone;
+                    tempCopy->neighbors.push_back(clone);
+                    q.push(n);
+                }
+                else{
+                    tempCopy->neighbors.push_back(mp[n]);
+                }
+            }
+
         }
-        return dfs(node,mp);
+
+
+        return copy;
     }
 };
