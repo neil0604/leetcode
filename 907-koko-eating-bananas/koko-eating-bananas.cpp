4cf86c0df85b128c36486673f377c593f48cb43b
neil0604 <snehildey13@gmail.com>
2025-09-25 18:21:18 +0530

    Time: 23 ms (6.23%) | Memory: 22.9 MB (42.46%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,30 +1,27 @@
 class Solution {
-public:
-    bool f(vector<int> & piles,int mid,int h){
-        long long total=0;
-        for(auto ele : piles){
-            total+=ceil((double)ele/mid);
+private:
+   unsigned long long f(vector<int>& piles, int mid){
+       unsigned long long sum=0;
+        for(auto pile : piles){
+          sum+=  ceil((double)pile/mid);
         }
-        return total <= h;
+        return sum;
     }
+public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi=0;
-        for(auto ele : piles){
-            maxi=max(maxi,ele);
-        }
+        if(h<piles.size()) return 0;
+
+        int low=1,high=1e9;
         int ans=0;
-        //ramge (1,maxi)
-        int n=piles.size();
-        int low=1,high=maxi;
+
         while(low<=high){
             int mid=(low+high)/2;
-
-            if(f(piles,mid,h)==true) {
-                ans=mid;
+         unsigned   long long required=f(piles,mid);
+            if(required<=h){
                 high=mid-1;
-
+                ans=mid;
             }
-            else {
+            else{
                 low=mid+1;
             }
         }
