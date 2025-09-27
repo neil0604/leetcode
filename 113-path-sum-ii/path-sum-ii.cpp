e32337ae932b3e83d71e32d29756739945d5d5c3
neil0604 <snehildey13@gmail.com>
2025-09-27 14:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (87.89%) - LeetSync

--- 113-path-sum-ii/path-sum-ii.cpp
+++ 113-path-sum-ii/path-sum-ii.cpp
@@ -11,32 +11,32 @@
  */
 class Solution {
 private:
-    void f(TreeNode* root,int sum,int target,vector<int> &ans,vector<vector<int>> & result){
+    void helper(TreeNode* root,int target,vector<int> &ans,vector<vector<int>>& res,int sum ){
+        if(root==NULL) return ;
         if(root->left==NULL && root->right==NULL){
-          if(  sum+root->val==target) {
+            // leaf node
             ans.push_back(root->val);
-            result.push_back(ans);
+            sum+=root->val;
+            if(sum==target){
+                res.push_back(ans);
+            }
+            sum-=root->val;
             ans.pop_back();
-            return ;
-          }
-         
+            return;
         }
-       // bool left=false,right=false;
-       ans.push_back(root->val);
-      if(root->left!=NULL)  f(root->left,sum+root->val,target,ans,result);
-      if(root->right!=NULL)  f(root->right,sum+root->val,target,ans,result);
-      ans.pop_back();
-
-      
+        // traversal
+        ans.push_back(root->val);
+        sum+=root->val;
+        helper(root->left,target,ans,res,sum);
+         helper(root->right,target,ans,res,sum);
+        sum-=root->val;
+        ans.pop_back();
     }
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        
-        if(root==NULL) return {};
         vector<int> ans;
-        vector<vector<int>> result;
-
-        f(root,0,targetSum,ans,result);
-        return result;
+        vector<vector<int>> res;
+        helper(root,targetSum,ans,res,0);
+        return res;
     }
 };
