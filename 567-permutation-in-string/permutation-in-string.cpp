d5c349004a4a67a4162959797b3add7a09708869
neil0604 <snehildey13@gmail.com>
2025-09-27 14:13:53 +0530

    Time: 1526 ms (5.11%) | Memory: 229.5 MB (5.01%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,33 +1,25 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        // find all permutations of string s1
-        //fill them in set 
-        //take a window of s1.size and check in set if present
-
-        // finding all permutation of s1
+        
         int n=s1.size();
         int m=s2.size();
-        unordered_map<char,int> mp;
-
-        for(auto c : s1){
+        unordered_map<int,int> mp;
+        int cnt=0;
+        for(char c : s1){
             mp[c]++;
         }
-        for(int i=0;i<=m-n;i++){
-            unordered_map<char,int> mpp=mp;
-           bool flag=true;
-            string check = s2.substr(i,s1.size());
-          //  cout<<check<<" ";
-            for(char c :  check){
-                mpp[c]--;
-            }
-            for(auto pair : mpp) {
-                if(pair.second != 0) {flag=false;
-                break;}
-            }
-            if(flag!=false) return true;
 
+        for(int i=0;i<=m-n;i++){
+            string str=s2.substr(i,n);
+            unordered_map<int,int> copy=mp;
+
+            for(auto ele : str){
+                copy[ele]--;
+                if(copy[ele]==0) copy.erase(ele);
+            }
+            if(copy.empty()) cnt++;
         }
-        return false;
+        return cnt;
     }
 };
