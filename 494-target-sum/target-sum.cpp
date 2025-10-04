7e9e2b06e4412d5b5f26992fb35679a0c2b6d713
neil0604 <snehildey13@gmail.com>
2025-10-05 00:35:45 +0530

    Time: 272 ms (30.01%) | Memory: 62 MB (8.04%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,20 +1,28 @@
 class Solution {
 private:
-    void helper(vector<int>& nums, int target,int idx, int sum,int & cnt){
+    int helper(vector<int>& nums, int target,int idx, int sum,int & cnt,map<pair<int, int>, int>& dp){
         // base case
         if(idx==nums.size()){
-            if(sum==target) cnt++;
-            return;
+          //  int a=0;
+            if(sum==target) return 1;
+            return 0;
         }
+        if(dp.count({idx,sum})) return dp[{idx,sum}];
 
         // operations
-        helper(nums,target,idx+1,sum+nums[idx],cnt);
-        helper(nums,target,idx+1,sum-nums[idx],cnt);
+       int add= helper(nums,target,idx+1,sum+nums[idx],cnt,dp);
+       int sub= helper(nums,target,idx+1,sum-nums[idx],cnt,dp);
+
+       return dp[{idx,sum}]=add+sub;
+
+
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
+       // dp map to store results of subproblems {index, sum} -> ways
+        map<pair<int, int>, int> dp;
         int cnt=0;
-        helper(nums,target,0,0,cnt);
+       return helper(nums,target,0,0,cnt,dp);
 
         return cnt;
     }
